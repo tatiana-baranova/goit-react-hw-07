34f2a33e7434cab834ed44cef849cff2428f88ab
tetiana_baranova <syniovych.tanya@gmail.com>
2025-03-11 22:47:02 +0200

    file contactsOps

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-items: [],
+    items: [],
+    loading: false,
+    error: null,
 };
 
 const contactsSlice = createSlice({
